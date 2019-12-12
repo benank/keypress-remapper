62858f4d545d1314e1a5cd3c7e6540e3ed2b8d6d
benank <mp.paradigm@gmail.com>
2019-12-12 15:47:35 -0800

    Finish script

--- main.js
+++ main.js
@@ -5,25 +5,34 @@ const keys =
 {
     17: {is_down: false}, // p1 up
     30: {is_down: false, remap: "mouse_left"}, // p1 left
-    31: {is_down: false}, // p1 down
+    31: {is_down: false, remap: "mouse_scroll"}, // p1 down
     32: {is_down: false, remap: "mouse_right"}, // p1 right
     23: {is_down: false, remap: "mouse_up"}, // p2 up
-    36: {is_down: false}, // p2 left
+    36: {is_down: false, remap: "mouse_leftclick"}, // p2 left
     37: {is_down: false, remap: "mouse_down"}, // p2 down
-    38: {is_down: false}, // p2 right
+    38: {is_down: false, remap: "mouse_rightclick"}, // p2 right
 }
 
 const mouse_traslations = {
-    "mouse_left": {x: -20, y: 0},
-    "mouse_right": {x: 20, y: 0},
-    "mouse_up": {x: 0, y: -20},
-    "mouse_down": {x: 0, y: 20}
+    "mouse_left": {x: -30, y: 0},
+    "mouse_right": {x: 30, y: 0},
+    "mouse_up": {x: 0, y: -30},
+    "mouse_down": {x: 0, y: 30}
 }
 
 ioHook.on('keydown', (event) => {
     if (keys[event.keycode] == null)
     {
         return;
+    }
+
+    if (keys[event.keycode].remap == "mouse_leftclick")
+    {
+        robot.mouseToggle("down", "left");
+    }
+    else if (keys[event.keycode].remap == "mouse_rightclick")
+    {
+        robot.mouseToggle("down", "right");
     }
 
     keys[event.keycode].is_down = true;
@@ -33,6 +42,19 @@ ioHook.on('keyup', (event) => {
     if (keys[event.keycode] == null)
     {
         return;
+    }
+
+    if (keys[event.keycode].remap == "mouse_leftclick")
+    {
+        robot.mouseToggle("up", "left");
+    }
+    else if (keys[event.keycode].remap == "mouse_rightclick")
+    {
+        robot.mouseToggle("up", "right");
+    }
+    else if (keys[event.keycode].remap == "mouse_scroll")
+    {
+        robot.scrollMouse(0, -1); // Doesn't work, use autohotkey :(
     }
 
     keys[event.keycode].is_down = false;
@@ -47,13 +69,20 @@ function MouseLoop()
     {
         if (keys[key].remap != null && keys[key].is_down == true)
         {
-            mousePos = {
-                x: mousePos.x + mouse_traslations[keys[key].remap].x,
-                y: mousePos.y + mouse_traslations[keys[key].remap].y}
+            if (mouse_traslations[keys[key].remap] != null)
+            {
+                mousePos = {
+                    x: mousePos.x + mouse_traslations[keys[key].remap].x,
+                    y: mousePos.y + mouse_traslations[keys[key].remap].y}
+            }
         }
     }
 
-    robot.moveMouse(mousePos.x, mousePos.y);
+    const origMousePos = robot.getMousePos();
+    if (mousePos.x != origMousePos.x || mousePos.y != origMousePos.y)
+    {
+        robot.moveMouse(mousePos.x, mousePos.y);
+    }
 
     setTimeout(() => {
         MouseLoop();
---
+++ scroll.ahk
@@ -0,0 +1,10 @@
+﻿S::
+Loop
+{
+    GetKeyState, state, S, P
+    if state = U
+        break
+    Send, {WheelDown}
+    Sleep, 100
+}
+return
